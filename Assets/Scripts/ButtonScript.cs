ea3d504b9e6de31230444f5260cee4aae448c42a
Andrew <andrey.arakelyan@bask.ws>
2020-04-28 21:26:56 +0300

    first commit

---
+++ Assets/Scripts/ButtonScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class ButtonScript : MonoBehaviour
+{
+
+    public GameObject gameObjectPanel;
+    public GameObject gameObjectImage;
+    public Sprite Newsprite;
+
+
+    // Start is called before the first frame update
+    public void Close()
+    {   
+        gameObjectPanel.SetActive(false);
+        
+    }
+
+    // Update is called once per frame
+    public void ChangeImage()
+    {
+        gameObjectImage.GetComponent<Image>().sprite = Newsprite;
+    }
+}
